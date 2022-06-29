8b70b8c3ccf938fc82a75b48dfe8ae282228e9e7
Julio Trecenti <julio.trecenti@gmail.com>
2022-06-29 20:45:07 -0300

    fazendo o setup do github action

--- inst/rodar-gpt3.R
+++ inst/rodar-gpt3.R
@@ -1,23 +1,7 @@
 token <- Sys.getenv("OPENAI_TOKEN")
 
-r <- httr::POST(
-  "https://api.openai.com/v1/completions",
-  httr::add_headers(
-    "Authorization" = paste("Bearer", token)
-  ),
-  body = list(
-    model = "text-davinci-002",
-    prompt = "Escreva um poema fofinho sobre github actions, em português.",
-    temperature = 0.7,
-    max_tokens = 512,
-    top_p = 1,
-    frequency_penalty = 0,
-    presence_penalty = 0
-  ),
-  encode = "json"
-)
+pkgload::load_all(".")
 
-cat(
-  httr::content(r)$choices[[1]]$text,
-  file = "README.md"
-)
+texto <- rodar_gpt3(token)
+
+cat(texto, file = "README.md")
